6c08fbe1dae14ec23748d97612481d142a947c6b
toshiya kawasaki <kawasakitoshiya@gmail.com>
2022-08-31 23:17:59 +0900

    use vars

--- models/materialized_dwh/employee_names.sql
+++ models/materialized_dwh/employee_names.sql
@@ -9,9 +9,10 @@ select
 	concat("first_name", ' ', "last_name") as full_name
 from
 	"dbt_training"."raw"."employees"
+where "employee_id" <> '{{ var("skip_employee_id") }}'
 
 {% if is_incremental() %}
 
-	where "employee_id" not in (select "employee_id" from {{ this }})
+	and "employee_id" not in (select "employee_id" from {{ this }})
 
 {% endif %}
