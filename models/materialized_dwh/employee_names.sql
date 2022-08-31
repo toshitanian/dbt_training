83cc13b49ddbf4a8c6665b4ca02c018fd5bb9e6a
toshiya kawasaki <kawasakitoshiya@gmail.com>
2022-09-01 00:00:43 +0900

    source

--- models/materialized_dwh/employee_names.sql
+++ models/materialized_dwh/employee_names.sql
@@ -7,8 +7,7 @@
 select
 	"employee_id",
 	concat("first_name", ' ', "last_name") as full_name
-from
-	"dbt_training"."raw"."employees"
+from {{ source('staging', 'employees') }}
 where "employee_id" <> '{{ var("skip_employee_id") }}'
 
 {% if is_incremental() %}
